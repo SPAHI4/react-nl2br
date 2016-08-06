959a3321639d2ba285b1d4b8c1ebfd999aefddef
Anton Suslov <spaxi4@yandex.ru>
2016-08-07 00:11:49 +0300

    No more than 2 <br> in a row

--- index.js
+++ index.js
@@ -9,10 +9,18 @@ module.exports = function(str) {
   } else if (typeof str !== 'string') {
     return '';
   }
+  
+  var current = 0;
 
   return str.split(newlineRegex).map(function(line, index) {
     if (line.match(newlineRegex)) {
-      return React.createElement('br', { key: index });
+      if (current < 2) {
+        React.createElement('br', { key: index });
+        current++;
+      } else {
+        current = 0;
+        return false;
+      }
     } else {
       return line;
     }
